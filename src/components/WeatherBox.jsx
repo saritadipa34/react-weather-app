c81b864149aea2c703edc640a18418eb5dddf549
saritadipa34 <saritadipa34@gmail.com>
2025-04-10 13:23:35 +0530

    Icon logic added

--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -1,13 +1,35 @@
 import { MdOutlineWaves } from "react-icons/md";
 import { TiWeatherPartlySunny } from "react-icons/ti";
+import { TiWeatherCloudy } from "react-icons/ti";
 import { FiWind } from "react-icons/fi";
+import { TiWeatherShower } from "react-icons/ti";
+import { TiWeatherSunny } from "react-icons/ti";
+import { useState } from "react";
 
 const WeatherBox=({weatherValue})=>{
+
 
     console.log(weatherValue);
     
 if(!weatherValue){
    return <div></div>
+}
+
+const weatherIcon =()=>{
+const iconValue=weatherValue.weather[0].main;
+switch (iconValue){
+    case 'Clouds':
+return <TiWeatherCloudy />;
+
+case 'Rain':
+return    <TiWeatherShower />;
+
+    case 'Clear':
+return      <TiWeatherSunny />;
+
+        default :
+        <TiWeatherPartlySunny />;
+}
 }
 
 const {name,wind,main}=weatherValue;
@@ -19,7 +41,7 @@ const temperature=(main.temp - 273.15).toFixed(1);
     return(
         <div className=" h-3/4 text-white text-center mt-[20px]">
             <div className="flex justify-center items-center mb-[40px]">
-            <TiWeatherPartlySunny color="white" size="90px"/>
+           <div className="text-6xl">{weatherIcon()}</div>
             </div>
 <h1 className="text-5xl font-semibold">{temperature}°C</h1>
 <h1 className="text-5xl font-semibold">{cityName}</h1>
