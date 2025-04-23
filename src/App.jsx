990284a42a69e9a8feb81c855b4a842841e756e8
saritadipa34 <saritadipa34@gmail.com>
2025-04-23 20:27:55 +0530

    Created InputBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import InputBox from "./components/InputBox";
 
 const App=()=>{
   return(
-    <div>
-      app
+    <div className="h-[430px] w-[380px] p-8 bg-blue-400">
+      <InputBox />
     </div>
   )
 }
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,7 @@
+
+const InputBox=()=>{
+    return(
+        <input className="h-12 w-3/4 border px-4 rounded-3xl" type="text" />
+    )
+}
+export default InputBox;
