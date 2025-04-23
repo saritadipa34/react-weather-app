c9d484fc783d6eed34edc91c026d916d46327467
saritadipa34 <saritadipa34@gmail.com>
2025-04-23 20:35:47 +0530

    Done with SearchBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import InputBox from "./components/InputBox";
+import SearchBox from "./components/SearchBox";
 
 const App=()=>{
   return(
     <div className="h-[430px] w-[380px] p-8 bg-blue-400">
+      <div className="flex justify-between">
       <InputBox />
+      <SearchBox />
+      </div>
     </div>
   )
 }
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,10 @@
+import { FaSearch } from "react-icons/fa";
+
+const SearchBox=()=>{
+    return(
+<div className="h-12 w-14 cursor-pointer border rounded-full flex items-center justify-center">
+<FaSearch />
+</div>
+    )
+}
+export default SearchBox;
