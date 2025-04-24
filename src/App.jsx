e85274b47a18312cf8cfc522041ffaca8f3e7019
saritadipa34 <saritadipa34@gmail.com>
2025-04-24 18:06:19 +0530

    Loading state added before fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,44 @@ import WeatherBox from "./components/WeatherBox";
 
 const App=()=>{
 const [inputValue,setInputValue]=useState('');
-const [weatherData,setWeatherData]=useState([]);
+const [weatherData,setWeatherData]=useState(null);
 const [search,setSearch]=useState("Bhubaneswar");
+const [loading,setLoading]=useState(false);
 
 const handleClick=()=>{
   setSearch(inputValue);
-  console.log("click")
+  setInputValue("");
 }
 
 const handleInput=(e)=>{
   const inputData=e.target.value;
   setInputValue(inputData);
-  console.log(e.target.value);
 }
 
 useEffect(()=>{
-  if(!inputValue) return;
+setLoading(true);
     const getData=async()=>{
+  try{ 
         const api_key=import.meta.env.VITE_API_KEY;
         const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${api_key}`);
         const data=await response.json();
-  
+          setWeatherData(data);
+          console.log(data);
 
-    setWeatherData(data);
-        console.log(data);
-    }
+          if(data.cod === 200){
+            setWeatherData(data);
+            setLoading(false);
+          }
+
+
+        } catch (error) {
+          setLoading(false);
+          console.log(error.message);
+        }
+    } 
     getData();
    
 },[search]);
-
 
   return(
     <div className="h-[430px] w-[380px] p-8 bg-blue-400">
@@ -41,7 +50,15 @@ useEffect(()=>{
       <InputBox onChange={handleInput} inputValue={inputValue} />
       <SearchBox onClick={handleClick}/>
       </div>
-    <WeatherBox inputValue={inputValue} setInputValue={setInputValue}/>
+    
+  {loading ? (<div className="text-center mt-25 text-white text-4xl">loading....</div> )
+  :
+
+  (weatherData &&
+    <WeatherBox loading={loading} setLoading={setLoading} weatherData={weatherData} inputValue={inputValue} setInputValue={setInputValue}/>
+     )
+  }
+  
     </div>
   )
 }
--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -2,24 +2,32 @@ import { TiWeatherPartlySunny } from "react-icons/ti";
 import { MdOutlineWaves } from "react-icons/md";
 import { FaWind } from "react-icons/fa";
 
-const WeatherBox=()=>{
+const WeatherBox=({weatherData})=>{
 
+const {name,main,wind}=weatherData;
+const Humidity=main.humidity;
+const city =name;
+const windSpeed=wind.speed;
+const temperature=main.temp;
+
+  
     return(
+
         <div>
             <div className="h-1/4 w-full flex my-7 items-center text-white justify-center">
               <TiWeatherPartlySunny size={"80px"} />
               </div>
-              <h1 className="text-5xl text-center font-bold">21°C</h1>
-              <h1 className="text-center font-bold text-3xl">NewYork</h1>
+              <h1 className="text-5xl text-center font-bold">{(temperature - 273.15).toFixed(1)}°C</h1>
+              <h1 className="text-center font-bold text-3xl">{city}</h1>
               <div className="flex my-7 justify-between">
                 <div>
                 <MdOutlineWaves />
-                <p>67%</p>
+                <p>{Humidity}%</p>
                 <p>Humidity</p>
                 </div>
                 <div>
                 <FaWind />
-                <p>2.06 km/h</p>
+                <p>{ windSpeed} km/h</p>
                 <p>Wind Speed</p>
                 </div>
               </div>
