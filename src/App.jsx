d7702933a5e516a4eb432953abbc27d6cd9278ef
saritadipa34 <saritadipa34@gmail.com>
2025-04-25 12:25:45 +0530

    Done with Error handling

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ const [inputValue,setInputValue]=useState('');
 const [weatherData,setWeatherData]=useState(null);
 const [search,setSearch]=useState("Bhubaneswar");
 const [loading,setLoading]=useState(false);
+const [error,setError]=useState(false);
+
 
 const handleClick=()=>{
+  if(inputValue.trim() === "")return;
   setSearch(inputValue);
   setInputValue("");
 }
@@ -21,22 +24,28 @@ const handleInput=(e)=>{
 
 useEffect(()=>{
 setLoading(true);
+setError(false);
     const getData=async()=>{
   try{ 
+    // if(inputValue.trim() === "")  return;
         const api_key=import.meta.env.VITE_API_KEY;
         const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${api_key}`);
         const data=await response.json();
-          setWeatherData(data);
           console.log(data);
 
           if(data.cod === 200){
             setWeatherData(data);
+            setError(false);
+            setLoading(false);
+          } else {
+            setError(true);
             setLoading(false);
           }
 
 
         } catch (error) {
           setLoading(false);
+          setError(true);
           console.log(error.message);
         }
     } 
@@ -50,13 +59,13 @@ setLoading(true);
       <InputBox onChange={handleInput} inputValue={inputValue} />
       <SearchBox onClick={handleClick}/>
       </div>
-    
+      {error && !loading && <p className="text-red-700 ml-18 text-sm">City Not Found</p>}
   {loading ? (<div className="text-center mt-25 text-white text-4xl">loading....</div> )
   :
 
-  (weatherData &&
-    <WeatherBox loading={loading} setLoading={setLoading} weatherData={weatherData} inputValue={inputValue} setInputValue={setInputValue}/>
-     )
+  (weatherData &&  (
+    <WeatherBox error={error} setError={setError} weatherData={weatherData} inputValue={inputValue} setInputValue={setInputValue}/>
+     ))
   }
   
     </div>
